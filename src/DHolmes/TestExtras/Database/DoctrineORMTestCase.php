d498a1a2f61bb7628b66f262b916b859ef38cf9f
Daniel Holmes <daniel@creatio.com.au>
2012-05-14 00:59:36 +1000

    tweaked fixtures management

--- src/DHolmes/TestExtras/Database/DoctrineORMTestCase.php
+++ src/DHolmes/TestExtras/Database/DoctrineORMTestCase.php
@@ -3,6 +3,7 @@
 namespace DHolmes\TestExtras\Database;
 
 use PHPUnit_Framework_TestCase;
+use InvalidArgumentException;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManager;
@@ -38,6 +39,12 @@ abstract class DoctrineORMTestCase extends PHPUnit_Framework_TestCase implements
     /** @return OperationsHelper */
     protected function createOperationsHelper()
     {
+        $helper = new OperationsHelper();
+        $helper->setIsSchemaCacheEnabled(true);
+        // Cannot really compare entities properly with this enabled because actual entity not 
+        // inserted in session. Might be possible if compare contents (excluding id and timestamps?)
+        // but that could be innacurate
+        $helper->setIsFixturesCacheEnabled(false);
         return OperationsHelper::createWithCachedSchemaAndFixtures();
     }
     
@@ -56,7 +63,14 @@ abstract class DoctrineORMTestCase extends PHPUnit_Framework_TestCase implements
      */
     protected function ensureEntityManaged($entity)
     {
-        return $this->getEntityManager()->merge($entity);
+        $em = $this->getEntityManager();
+        $merged = $em->merge($entity);
+        if ($em->getUnitOfWork()->isScheduledForInsert($merged))
+        {
+            $desc = $this->getEntityDescription($entity);
+            throw new InvalidArgumentException(sprintf('Entity %s not within manager', $desc));
+        }
+        return $merged;
     }
     
     /**
@@ -70,8 +84,30 @@ abstract class DoctrineORMTestCase extends PHPUnit_Framework_TestCase implements
         $expectedEntity = $entityManager->merge($expectedEntity);
         $entity = $entityManager->merge($entity);
         
-        $this->assertSame($expectedEntity, $entity, 
-            sprintf('Entities not the same (%s) (%s)', get_class($expectedEntity), get_class($entity)));
+        $expectedDesc = $this->getEntityDescription($expectedEntity);
+        $entityDesc = $this->getEntityDescription($entity);
+        
+        $assertMessage = sprintf('Entities not the same:' . "\n" . '  %s' . "\n" . '  %s', 
+                            $expectedDesc, $entityDesc);
+        $this->assertSame($expectedEntity, $entity, $assertMessage);
+    }
+    
+    /**
+     * @param object $entity
+     * @return string
+     */
+    private function getEntityDescription($entity)
+    {
+        $entityString = null;
+        if (method_exists($entity, '__toString'))
+        {
+            $entityString = (string)$entity;
+        }
+        else
+        {
+            $entityString = spl_object_hash($entity);
+        }
+        return sprintf('%s <%s>', $entityString, get_class($entity));
     }
     
     /**
